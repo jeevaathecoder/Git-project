c098037f4d0a85fe39303be5664f446678aab4b2
GOWTHAM .P <140637780+GOWTHAM6233@users.noreply.github.com>
2023-09-19 12:58:25 +0530

    Add files via upload

---
+++ Git/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args) {
+      System.out.println("Hello")  ;
+      System.out.println("Hey");
+    System.out.println("everyone");
+    System.out.println("good");
+    System.out.println("hey git");
+    System.out.println("guru");
+
+    }
+  }
