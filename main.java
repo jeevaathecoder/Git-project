25ab8951cfd1b1efbc2a3120467dea6857863446
Dareddy Swathi <swathidareddy5@gmail.com>
2023-09-19 14:21:16 +0530

    jansitha

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("hello git");
+        int a=5;
+        int b=6;
+        int c=a+b;
+        System.out.println("sum of "+c);
     }
 }
